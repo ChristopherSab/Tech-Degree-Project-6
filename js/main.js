ec52e8c9883829e6b4dd60d14662ba123863f2b3
ChristopherSab <chris.sabaoth@protonmail.com>
2020-03-23 17:54:13 +0000

    Created function that generates random phrase array

--- js/main.js
+++ js/main.js
@@ -6,7 +6,6 @@ const startGame = document.getElementById('btn__reset');
 let incorrectGuesses = 0;
 
 // An array of cliche phrases
-
 const phrasesArray = [
     "Love is Blind",
     "What goes around comes around",
@@ -14,6 +13,20 @@ const phrasesArray = [
     "Jack of all trades",
     "You cannot cry over spilt milk"
 ];
+
+// This function gets a random phrase as an array
+function getRandomPhraseAsArray(arr) {
+  
+    const arrayIndex = Math.floor( Math.random() * arr.length) + 1;
+
+    return arr[arrayIndex].split('');
+
+}
+
+
+
+
+
 
 
 
