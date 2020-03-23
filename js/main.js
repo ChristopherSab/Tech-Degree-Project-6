88a9b9e012f079382cdf1758305eb448bfc2abdf
ChristopherSab <chris.sabaoth@protonmail.com>
2020-03-23 14:30:20 +0000

    Added some of the main Js Variables

---
+++ js/main.js
@@ -0,0 +1,7 @@
+
+// Variables //
+const keyboardButtons = document.getElementById('qwerty');
+const phrase = document.getElementById('phrase');
+const startGame = document.getElementById('btn__reset');
+let incorrectGuesses = 0;
+
