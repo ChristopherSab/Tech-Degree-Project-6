d389837714af46c4e2998065dd4c424de2394cbb
ChristopherSab <chris.sabaoth@protonmail.com>
2020-03-23 14:51:17 +0000

    Added phrases array

--- js/main.js
+++ js/main.js
@@ -5,3 +5,15 @@ const phrase = document.getElementById('phrase');
 const startGame = document.getElementById('btn__reset');
 let incorrectGuesses = 0;
 
+// An array of cliche phrases
+
+const phrasesArray = [
+    "Love is Blind",
+    "What goes around comes around",
+    "Everything that glitters is not gold",
+    "Jack of all trades",
+    "You cannot cry over spilt milk"
+];
+
+
+
